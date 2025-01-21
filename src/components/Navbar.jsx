68c7a89a907c9a26013f07599fda37fb326395d5
ant-shubhamkaushal <shubham.kaushal@antcreatives.com>
2025-01-21 15:57:37 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ToggleButton from "./ToggleButton";
 import { useNavigate } from "react-router-dom";
-
+import { FaSearch } from "react-icons/fa";
 function Navbar({ toggleDarkMode, input, SetInput, handelSearch }) {
   const navigate = useNavigate();
 
@@ -38,14 +38,18 @@ function Navbar({ toggleDarkMode, input, SetInput, handelSearch }) {
           </button>
 
           {/* Search Input */}
-          <input
-            type="search"
-            placeholder="Search here"
-            value={input}
-            onChange={(e) => SetInput(e.target.value)}
-            className="w-full lg:w-[300px] py-2 px-4 border border-gray-400 text-sm rounded-lg outline-none text-slate-950 dark:bg-gray-800 dark:text-gray-200 dark:border-gray-600 focus:ring-2 focus:ring-green-400"
-          />
-          <button onClick={() => handelSearch()}>click</button>
+          <div className="relative w-full lg:w-[300px]">
+            <input
+              type="search"
+              placeholder="Search here"
+              value={input}
+              onChange={(e) => SetInput(e.target.value)}
+              className="w-full px-4 py-2 pl-10 text-sm border border-gray-400 rounded-lg outline-none text-slate-950 dark:bg-gray-800 dark:text-gray-200 dark:border-gray-600 focus:ring-2 focus:ring-green-400"
+            />
+            <span onClick={handelSearch}>
+              <FaSearch className="absolute text-gray-400 transform -translate-y-1/2 left-3 top-1/2 dark:text-gray-200" />
+            </span>
+          </div>
         </div>
       </nav>
     </div>
